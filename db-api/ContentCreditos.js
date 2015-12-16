42ffd6333c7df6b073b7f1ce0ed4a347ce7e7926
solo inno <nestorsuat@gmail.com>
2015-12-16 16:48:24 -0500

    correccion fix css

--- db-api/ContentCreditos.js
+++ db-api/ContentCreditos.js
@@ -1,56 +1,56 @@
 export default[
 	{
 		id: 1,
-		rol: 'INVESTIGADOR PRINCIPAL',		
+		rol: 'INVESTIGADOR PRINCIPAL',
 		nombre: 'Oscar Agudelo Varela',
 		informacion: 'Msc. Ciencias de la Información y las Comunicaciones',
 		avatar: 'n'
 	},
 	{
 		id: 5,
-		rol: 'AUXILIAR INVESTIGADOR',		
+		rol: 'AUXILIAR INVESTIGADOR',
 		nombre: 'Miguel M. Bonilla Morales',
 		informacion: 'Lic. Producción Agropecuaria',
 		avatar: 'n'
 	},
 	{
 		id: 6,
-		rol: 'DESARROLLADOR DE SOFTWARE' ,		
+		rol: 'DESARROLLADOR DE SOFTWARE' ,
 		nombre: 'Néstor E. Suat Rojas',
 		informacion: 'Estudiante Ingeniería de Sistemas',
 		avatar: 'n'
 	},
 	{
 		id: 3,
-		rol: 'DESARROLLADOR DE SOFTWARE',		
+		rol: 'DESARROLLADOR DE SOFTWARE',
 		nombre: 'David R. Grandas C.',
 		informacion: 'Estudiante de Ingeniería de Sistemas',
 		avatar: 'n'
-	},		
+	},
 	{
 		id: 8,
-		rol: 'Unillanos',		
+		rol: 'Unillanos',
 		nombre: 'Universidad de los Llanos',
 		informacion: 'logo',
 		avatar: 'public/img/logo_unillanos.png'
 	},
 	{
 		id: 3,
-		rol: 'DERECHOS RESERVADOS',		
-		nombre: 'Primera Edición, Villavicentio - Meta',
+		rol: 'DERECHOS RESERVADOS',
+		nombre: 'Primera Edición, Villavicencio - Meta',
 		informacion: 'UNIVERSIDAD DE LOS LLANOS',
 		avatar: 'n'
-	},	
+	},
 	{
 		id: 1,
-		rol: 'APROBADO Y FINANCIADO POR',		
+		rol: 'APROBADO Y FINANCIADO POR',
 		nombre: 'Dirección general de investigaciones',
 		informacion: 'UNIVERSIDAD DE LOS LLANOS',
 		avatar: 'n'
-	},	
+	},
 	{
 		id: 8,
-		rol: 'Grupo mediatico',		
+		rol: 'Grupo mediatico',
 		nombre: 'Grupo de Investigación Horizonte Mediatico',
 		informacion: 'logo',
 		avatar: 'public/img/logo_grupo.png'
